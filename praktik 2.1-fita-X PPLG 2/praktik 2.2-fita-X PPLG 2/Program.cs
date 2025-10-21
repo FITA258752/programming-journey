d9a8e18b344fcfd218f4790050daf768ae48e425
FITA258752 <amalia258752@student.smkn1kandeman.sch.id>
2025-10-21 09:51:27 +0700

    Add files via upload

---
+++ praktik 2.1-fita-X PPLG 2/praktik 2.2-fita-X PPLG 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_2._1_fita_X_PPLG_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // bilangan desimal dan karakter
+            double tinggi_badan = 175.5;
+            char jk = 'L';
+
+            Console.WriteLine("Tinggi Badan :" + tinggi_badan);
+            Console.WriteLine("Jenis kelamin:" + jk);
+        }
+    }
+}

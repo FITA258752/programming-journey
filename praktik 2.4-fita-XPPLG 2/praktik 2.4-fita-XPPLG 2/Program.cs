662ebb4ad187d1b2b20863952ff3ce1e044b3db7
FITA258752 <amalia258752@student.smkn1kandeman.sch.id>
2025-10-21 09:56:54 +0700

    Add files via upload

---
+++ praktik 2.4-fita-XPPLG 2/praktik 2.4-fita-XPPLG 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_2._4_fita_XPPLG_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarasi variabel
+            string nama_siswa = "Rizki Ulinuha";
+            string jk = "Laki-Laki";
+            int umur = 16;
+            int nilai = 85;
+            char grade = 'B';
+            double tb = 170.5;
+            bool lulus = true;
+
+            // menampilkan variabel
+            Console.WriteLine("Nama Siswa   :" + nama_siswa);
+            Console.WriteLine("Jenis Kelamin:" + jk);
+            Console.WriteLine("Umur         :" + umur);
+            Console.WriteLine("Nilai        :" + nilai);
+            Console.WriteLine("Grade nilai  :" + grade);
+            Console.WriteLine("Tinggi badan :" + tb + "cm");
+            Console.WriteLine("Lulus        :" + lulus);
+        }
+    }
+}

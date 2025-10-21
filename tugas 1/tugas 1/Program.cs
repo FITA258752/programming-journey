57c0708001acd47f1ce84053e0ca64c7bb9096fe
FITA258752 <amalia258752@student.smkn1kandeman.sch.id>
2025-10-21 14:48:47 +0700

    Add files via upload

---
+++ tugas 1/tugas 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_1
+{
+    internal class Program
+    {
+        // Fungsi Kali: menerima tiga bilangan dan menampilkan hasil perkalian
+        static void Kali(double a, double b, double c)
+        {
+            double hasil = a * b * c;
+            Console.WriteLine("Hasil perkalian dari {0}, {1}, dan {2} adalah: {3}", a, b, c, hasil);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Program Perkalian Tiga Bilangan ===");
+
+            Console.Write("Masukkan bilangan pertama: ");
+            double angka1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan bilangan kedua: ");
+            double angka2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan bilangan ketiga: ");
+            double angka3 = Convert.ToDouble(Console.ReadLine());
+
+            // Panggil fungsi Kali
+            Kali(angka1, angka2, angka3);
+        }
+    }
+}
